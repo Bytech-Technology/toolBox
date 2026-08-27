01947923dc839d4445754323c1aa78afd37ecf07
Bytech-Technology <bytechsuppor@gmail.com>
2026-08-27 05:15:03 -0300

    Add initial server setup with Express and EJS view engine

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -7,6 +8,7 @@ dotenv.config();
 const Routes = require('./routes/routes');
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
 app.use('/', Routes);
